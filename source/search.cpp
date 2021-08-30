63738a3a8d85bc2ca7e9ebbd46eaf16c15504e63
b00902076 <b00902076@csie.ntu.edu.tw>
2021-08-30 22:28:47 +0900

    deprecate the feature of printing result on the stdout

--- source/search.cpp
+++ source/search.cpp
@@ -18,7 +18,6 @@ void search(unordered_map<wstring, unordered_set<int>> &index, vector<wstring> &
     bool silent_mode = (args.count("-m=s")>0 || args.count("--mode=silent")>0);
     bool sort_by_hits = (args.count("-s=h")>0 || args.count("--sort=hit")>0);
     bool sort_by_id = (args.count("-s=i")>0 || args.count("--sort=id")>0);
-    bool print_result = (args.count("-p")>0 || args.count("--print-result")>0);
     wstring line;
     unordered_map<int,int> results_raw; // [{record_id, hit_count}, ...]
     vector<pair<int,int>> results_sort; // [{record_id, hit_count}, ...]
@@ -46,24 +45,18 @@ void search(unordered_map<wstring, unordered_set<int>> &index, vector<wstring> &
             sort(results_sort.begin(), results_sort.end(), sort_method);
         }
 
-        if(print_result){
-            for(auto &[record_id, hit_count]: results_sort)
-                wcout << records[record_id] << endl;
-            wcout << L"検索結果計 " << results_sort.size() << L"件" << endl;
+        wofstream search_result(_SEARCH_RESULT_FILENAME);
+        search_result.imbue(locale("C.UTF-8"));
+        for(auto &[record_id, hit_count]: results_sort)
+            search_result << records[record_id] << endl;
+        search_result.close();
+        if(silent_mode){
+            wcout.clear();
+            wcout << L"計 " << results_sort.size();
+            wcout << L" 件の検索結果を「.\\search_result」にて確認できます。" << endl;
+            wcout.setstate(ios_base::failbit);
         }
-        else{
-            wofstream search_result(_SEARCH_RESULT_FILENAME);
-            search_result.imbue(locale("C.UTF-8"));
-            for(auto &[record_id, hit_count]: results_sort)
-                search_result << records[record_id] << endl;
-            search_result.close();
-            if(silent_mode){
-                wcout.clear();
-                wcout << L"計 " << results_sort.size();
-                wcout << L" 件の検索結果を「.\\search_result」にて確認できます。" << endl;
-                wcout.setstate(ios_base::failbit);
-            }
-        }
+        
         wcout << L"検索キーワードを入力（終了したい場合はexitを入力）：";
     }
     
