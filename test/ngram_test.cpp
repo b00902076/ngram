bca841f744562431c33d0591582f8f136456a0aa
yellowweak <b00902076@csie.ntu.edu.tw>
2021-10-14 05:35:59 +0900

    first workable gtest

---
+++ test/ngram_test.cpp
@@ -0,0 +1,14 @@
+#include "../include/main.hpp"
+#include <gtest/gtest.h>
+// #include "lib/ngram/config.hpp"
+// #include <lib/ngram/initializer.hpp>
+
+// Config
+
+TEST(ConfigTest, DefaultValue) {
+    Config config;
+    char ** mock_argv = nullptr;
+    Initializer initializer(0, mock_argv);
+    EXPECT_EQ(config.mute_stdout, false) << L"default config(mute_stdout) incorrect";
+    EXPECT_EQ(config.sort_method, 0) << L"default config(sort_method) incorrect";
+}
