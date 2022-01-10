db935a26553822e16d4e71eaf18de07618312bf3
yellowweak <b00902076@csie.ntu.edu.tw>
2022-01-10 16:01:13 +0900

    add test on output files

--- test/main.cpp
+++ test/main.cpp
@@ -76,10 +76,21 @@ INSTANTIATE_TEST_CASE_P(
 );
 
 // class indexer
-TEST_F(IndexerTest, Indexer){
+TEST_F(IndexerTest, AllFeatures){
     Indexer indexer;
-    EXPECT_EQ(indexer.csv.size(), 124223) << L"Concatened csv is not correct";
-    EXPECT_EQ(indexer.index.size(), 54855) << L"Index loaded is not correct";
+    // test concatened csv
+    EXPECT_EQ(indexer.csv.size(), 124223) << L"CSV(runtime variable) is not correct";
+    ifstream csv(_CSV_CONCATENED_PATH_TEST);
+    int csv_line_cnt = std::count(std::istreambuf_iterator<char>(csv), std::istreambuf_iterator<char>(), '\n');
+    EXPECT_EQ(csv_line_cnt, 124223) << L"CSV doesn't have expected lines.";
+    csv.close();
+
+    // test index file
+    EXPECT_EQ(indexer.index.size(), 54855) << L"Index loaded(runtime variable) is not correct";
+    ifstream index(_INDEX_PATH_TEST);
+    int index_line_cnt = std::count(std::istreambuf_iterator<char>(index), std::istreambuf_iterator<char>(), '\n');
+    EXPECT_EQ(index_line_cnt, 54855) << L"Index file doesn't have expected lines.";
+    index.close();
 }
 
 int main(int argc, char **argv) {
