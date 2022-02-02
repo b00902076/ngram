e742c2809007e9e2a286cb755b82125105ffe734
yellowweak <b00902076@csie.ntu.edu.tw>
2022-02-02 22:05:10 +0900

    fixed wrong expexted value

--- test/ngram/indexer.hpp
+++ test/ngram/indexer.hpp
@@ -14,7 +14,7 @@ TEST_F(IndexerTest, Overall){
     EXPECT_EQ(indexer.csv.size(), 124223) << L"CSV(runtime variable) is not correct";
     ifstream csv(_CSV_CONCATENED_PATH_TEST);
     int csv_line_cnt = std::count(std::istreambuf_iterator<char>(csv), std::istreambuf_iterator<char>(), '\n');
-    EXPECT_EQ(csv_line_cnt, 1242230) << L"CSV doesn't have expected lines.";
+    EXPECT_EQ(csv_line_cnt, 124223) << L"CSV doesn't have expected lines.";
     csv.close();
 
     // test index file
